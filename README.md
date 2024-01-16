b0a4d867a17dcab56f15f0e00d029f296005df80
8Lou <lou8@mail.ru>
2024-01-16 10:39:09 +0300

    start

---
+++ README.md
@@ -0,0 +1,7 @@
+# https://8Lou.github.io/layout_to_React
+
+## Getting Started with Create React App
+
+### `npm start`
+### `npm test`
+### `npm run build`
---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div className="">
+      <p>
+        auto-leasing_lou
+      </p>
+      
+    </div>
+  );
+}
+
+export default App;
